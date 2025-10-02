9497ad91e9b338bcbb0e7ddeda5f45a9a21aca91
qasimwarraich <37380474+qasimwarraich@users.noreply.github.com>
2025-10-02 18:11:20 +0200

    Add some more about text and awareness concept

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -1,12 +1,28 @@
 <div>
-    <p class="text-2xl">
-        RAD.AU is your neighbourhood bicycle workshop collective and open space.
-    </p>
+    <div class="text-2xl">
+        <span class="inline-flex font-bold">
+            <span class="text-red-600">R</span>
+            <span class="text-orange-600">A</span>
+            <span class="text-yellow-300">D</span>
+            <span class="text-black">.</span>
+            <span class="text-green-950">A</span>
+            <span class="text-blue-900">U</span>
+            <span class="text-purple-950">!</span>
+        </span>
+        is your neighbourhood bicycle workshop collective and open space.
+    </div>
     <p>
+        <br />
         Our concept is simple: we have the tools, you have the hands, and if you
-        need help, we will try our best. We also have a collection of new and
-        used parts. Everything works over donations (prix libre/kollekte), so
-        please give as much as possible for you.
+        need help, we will try our best. We also have a collection of new and used
+        parts. Everything works over donations (prix libre/kollekte), so please give
+        as much as possible for you.
+        <br /> <br />
+
+        Outside of the workshop, we also want to offer a non capitalistic space
+        for people to socialise and do projects without any expectations of
+        consumption or any sense of exclusivity.
+
         <br /> <br />
 
         We are also known for organising alleycats, parties, concerts, and more.
@@ -14,7 +30,13 @@
         <a href="https:/instagram.com/rad_au/" target="_blank">instagram page</a
         >
         or our
-        <a href="https://t.me/rad_au/" target="_blank">telegram channel</a> to see
-        what's coming up!
+        <a href="https://t.me/rad_au/" target="_blank">telegram channel</a> to
+        see what's coming up!
+
+        <br /> <br />
+
+        We look forward to welcoming you at RAD.AU (map below)! Please take a
+        moment to read our
+        <a href="/docs/radau-awareness.pdf">awareness concept</a> and, see you soon!
     </p>
 </div>
