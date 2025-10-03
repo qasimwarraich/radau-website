382aaa21ff5f9124467c8e0e09e9fe4a41120cb0
qasimwarraich <37380474+qasimwarraich@users.noreply.github.com>
2025-10-03 14:17:24 +0200

    Update to Svelte 5 and SvelteKit 2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,19 @@
 import Header from "./Header.svelte";
 import Footer from "./Footer.svelte";
 import "./styles.css";
+/**
+ * @typedef {Object} Props
+ * @property {import('svelte').Snippet} [children]
+ */
+
+/** @type {Props} */
+let { children } = $props();
 </script>
 
 <div class="bg-gray-300 w-full max-w-full overflow-x-hidden">
     <Header />
     <main class="mx-[10%] xl:mx-[30%] min-h-screen">
-        <slot />
+        {@render children?.()}
     </main>
 </div>
 <Footer />
